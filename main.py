7ae24fc44f73f23ed70a0e430b7cd3be93848eb2
Michael Primavera <pastasauce59@gmail.com>
2023-03-10 16:50:28 -0500

    adjusted screen setup width and height, created a opp screen prompt for user to take a bet, created first turtile that starts at the left edge of screen but adjust x and y coordinates.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from turtle import Turtle, Screen()
-
-turtle1 = Turtle()
-
+from turtle import Turtle, Screen
 
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make your bet', prompt='Which turtle will win the race? Enter a color:')
+print(user_bet)
+
+turtle1 = Turtle(shape='turtle')
+turtle1.penup()
+turtle1.goto(x=-230, y=0)
+
+
 screen.exitonclick()
