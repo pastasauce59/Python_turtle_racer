ba1524591e3973c11fd5973d39b152006e8c3799
Michael Primavera <pastasauce59@gmail.com>
2023-03-10 17:01:51 -0500

    created 6 turtles for each color in colors list by ccreating a for in loop and using a while loop to start them all the 'starting line' at the edge of the window.

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title='Make your bet', prompt='Which turtle will win the race? Enter a color:')
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
-turtle1 = Turtle(shape='turtle')
-turtle1.penup()
-turtle1.goto(x=-230, y=0)
+# turtle1 = Turtle(shape='turtle')
+# turtle1.penup()
+# turtle1.goto(x=-230, y=0)
 
+yaxis = 0
+while yaxis != 6 * 30:
+    for color in colors:
+        turtle = Turtle(shape='turtle')
+        turtle.color(color)
+        turtle.penup()
+        turtle.goto(x=-230, y=yaxis)
+        yaxis += 30
 
 screen.exitonclick()
