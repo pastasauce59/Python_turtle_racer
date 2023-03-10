56601bf966b11568dd5c509650a1c63a22442a6c
Michael Primavera <pastasauce59@gmail.com>
2023-03-10 18:15:30 -0500

    created all_turtles list to append created turtle classes from for in loop, created if statement to catch when winning turtle reaches end of screen, then stops race and tells if the user has won or lost their bet.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title='Make your bet', prompt='Which turtle will win the race? Enter a color:')
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-
-# turtle1 = Turtle(shape='turtle')
-# turtle1.penup()
-# turtle1.goto(x=-230, y=0)
+all_turtles = []
 
 yaxis = 0
 while yaxis != 6 * 30:
@@ -16,6 +14,24 @@ while yaxis != 6 * 30:
         turtle.color(color)
         turtle.penup()
         turtle.goto(x=-230, y=yaxis)
+        all_turtles.append(turtle)
         yaxis += 30
+
+#Checking if user_bet exists
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost :( The {winning_color} turtle is the winner!")
+        
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
